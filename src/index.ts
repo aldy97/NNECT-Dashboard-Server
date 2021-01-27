ee6bf4aec1ed7dda86c02b34af945706b481ba29
Feng Xiong <fengxiong34@gmail.com>
2021-01-27 12:08:59 -0800

    changed cors origin

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: ['http://localhost:7000', 'https://nnect-dashboard.netlify.app'],
+        origin: ['http://localhost:3000', 'https://nnect-dashboard.netlify.app'],
     })
 );
 
